16ee01a4efa5b0e25b9c1f938e0e242fcd58d764
Migs23 <use.if1001guidovanrossum63print@gmail.com>
2026-01-02 13:37:08 -0300

    modified:   main.py
    Script completo, com as devidas corrções

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 # biblioteca necessária
 from moviepy import VideoFileClip, TextClip, CompositeVideoClip
+import time
+import os 
+
+# variavel contendo o arquivo original
+caminho_video_original = r"/home/migs/Documentos/estudos/video-cut/video_origin/3continentes-96---será-que-o-natal-em-cada-continente-é-diferente.mp4"
+caminho_video_corte = r"/home/migs/Documentos/estudos/video-cut/video_cut"
+os.makedirs(caminho_video_corte, exist_ok=True)
 
 
-video_original = (
-    VideoFileClip(r"/home/migs/Documentos/estudos/video-cut/video_origin/3continentes-96---será-que-o-natal-em-cada-continente-é-diferente.mp4")
-    .subclipped(10, 70)
-    .with_volume_scaled(0.8)
-)
+# constantes
+video = VideoFileClip(caminho_video_original)
+duracao_total = video.duration
+duracao_corte = 60
+numero_cortes = int(duracao_total // duracao_corte)
 
+print(f"Iniciando os {numero_cortes} cortes")
 
-# salvar video:
-resultado = CompositeVideoClip([video_original])
-resultado.write_videofile(r"/home/migs/Documentos/estudos/video-cut/video_cut/natal.mp4")
+# loop para a produção dos cortes:
+for i in range(numero_cortes):
+    inicio = i * duracao_corte
+    fim = inicio + duracao_corte
+
+    print(f"Corte {i+1}/{numero_cortes}")
+          
+    # carregar o video original dentro do loop a cada corte:
+    video_original = VideoFileClip(caminho_video_original)
+    
+    # criar os cortes
+    corte = video_original.subclipped(inicio, fim)
+
+    # salvar
+    corte.write_videofile(os.path.join(caminho_video_corte, f"natal_{i+1:03d}.mp4"))
+
+    # Liberar memória
+    corte.close()
+    video_original.close()
+
+    # Esperar 2 segundos
+    if i < numero_cortes - 1:  # Não esperar após o último
+        print("Aguardando 2 segundos...")
+        time.sleep(2)
+    print("Cortes realizados")
+
+print("Todos os cortes concluídos!")
