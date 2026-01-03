929c28182df66ccc9f2892552afed795dab38ccd
Migs23 <use.if1001guidovanrossum63print@gmail.com>
2026-01-03 19:36:27 -0300

    modified:   main.py modified:   README.md
    Script de corte completo! O erro envolvendo o tempo restante dos videos foi corrigido.
    Agora, o tempo restante é exibido corretamente após o termino dos cortes dos vídeos.
    
    README atualizado, contendo agora, o plano de desenvolvimento futuro.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ duracao_corte = 60
 numero_cortes = int(duracao_total // duracao_corte)
 
 print(f"Iniciando os {numero_cortes} cortes")
-
+"""
 # loop para a produção dos cortes:
 for i in range(numero_cortes):
     inicio = i * duracao_corte
@@ -42,5 +42,30 @@ for i in range(numero_cortes):
         print("Aguardando 2 segundos...")
         time.sleep(2)
     print("Cortes realizados")
+"""
+# Ultima etapa, pegar o ultimo corte (podemos chamar de sobra)
+tempo_total_cortes = duracao_corte * numero_cortes
+# loop para pegar o ultimo pedaço do video:
+for i in range(1):
+    inicio = tempo_total_cortes
+    fim = duracao_total
+    # carregar o video original dentro do loop a cada corte:
+    video_original = VideoFileClip(caminho_video_original)
+    
+    # criar os cortes
+    corte = video_original.subclipped(inicio, fim)
+
+    # salvar
+    corte.write_videofile(os.path.join(caminho_video_corte, "natal_57.mp4"))
+
+    # Liberar memória
+    corte.close()
+    video_original.close()
+
+    # Esperar 2 segundos
+    if i < numero_cortes - 1:  # Não esperar após o último
+        print("Aguardando 2 segundos...")
+        time.sleep(2)
+    print("Corte da sobra realizada")
 
 print("Todos os cortes concluídos!")
